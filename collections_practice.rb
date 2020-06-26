798eb28034086eb23d12d2fb2699d9847a2ae114
Charlie Barth <charliepbarth@gmail.com>
2020-06-26 18:04:45 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -29,3 +29,13 @@ def sort_array_char_count(array)
   end
 end
 
+def swap_elements_from_to(array)
+  array.each do |a|
+    if array.index("#{a}") == 1
+      array.index("#{a}") + 1 
+    elsif array.index("#{a}") == 2
+      array.index("#{a}") + -1 
+    else
+    end
+  end
+end
