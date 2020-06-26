1cf60dba0d22fb7183ef8b54d526cd95c92ab7ec
Charlie Barth <charliepbarth@gmail.com>
2020-06-26 18:29:16 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,11 +1,3 @@
-
-def swap_elements_from_to(array)
-  array.each do |a|
-    if array.index("#{a}") == 1
-      array.index("#{a}") + 1 
-    elsif array.index("#{a}") == 2
-      array.index("#{a}") + -1 
-    else
-    end
-  end
+def reverse_array(array)
+  array.reverse
 end
