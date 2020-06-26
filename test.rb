21c0b9a58d72bb0a7f790485d42ad390f6018c1c
Charlie Barth <charliepbarth@gmail.com>
2020-06-26 18:58:58 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,15 +1,2 @@
-def kesha_maker(array)
-  new_array = []
-  array.each do |word|
-  n = word.length
-  i = 2
-    while i < n
-      word[i] = "$"
-      i += 3
-      new_array << word
-    end
-  end
-  puts new_array
-end
-kesha_maker(["Kesha", "Style"])
-
+def find_a(array)
+  array.select
