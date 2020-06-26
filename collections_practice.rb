74e18bb8a65301630de42c4280e7b3b12adae6f5
Charlie Barth <charliepbarth@gmail.com>
2020-06-26 18:53:22 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -52,12 +52,9 @@ end
 def kesha_maker(array)
   new_array = []
   array.each do |word|
-  n = word.length
-  i = 2
-    while i < n
-      word[i] = "$"
-      i += 3
-      new_array << word
+    i = 2
+    word[i] = "$"
+    new_array << word
     end
   end
   new_array
