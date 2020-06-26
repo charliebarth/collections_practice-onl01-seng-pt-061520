47767eb7bdcd5b9db0955a8faae6cfacac37c6ea
Charlie Barth <charliepbarth@gmail.com>
2020-06-26 17:57:34 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -4,6 +4,7 @@ def swap_elements_from_to(array, index, destination_index)
   array.each do |a|
     if array.index("#{a}") == index
       puts array.index("#{a}") = destination_index
+    end
     else
   end
 end
