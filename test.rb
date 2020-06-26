c4d827bd7148f53abd9b927ad925a117e8feac79
Charlie Barth <charliepbarth@gmail.com>
2020-06-26 17:49:43 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -3,4 +3,6 @@ array.each_with_index do |a, b|
   if b = 1 or 2
 end
 
-def swap_elements_from_to(array, index, )
+def swap_elements_from_to(array, index, destination_index)
+  array.each do |a|
+    if 
