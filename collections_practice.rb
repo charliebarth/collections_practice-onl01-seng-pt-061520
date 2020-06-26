b3ea8f983601cbf7a6ee1861c7154b8a7f8b886e
Charlie Barth <charliepbarth@gmail.com>
2020-06-26 17:18:39 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,30 @@
+def sort_array_asc(array)
+  array.sort
+ 
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    if a < b 
+      1 
+    elsif a > b
+    -1
+  elsif a == b 
+  0 
+  else 
+  end
+ end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    if a.size < b.size
+      -1 
+    elsif a.size > b.size
+    1
+  elsif a.size == b.size
+  0 
+  else 
+  end
+end
+end
