1f88ed58e11d4d446b65761e6f48563bbe341ba4
Charlie Barth <charliepbarth@gmail.com>
2020-06-26 19:05:48 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,1 +1,8 @@
-puts [1,2].each_with_index.collect{|element, index| }
+def add_s(array)
+  new_array = []
+  array.each do |word|
+    word << "s"
+    new_array << word
+  end
+  puts new_array
+end
