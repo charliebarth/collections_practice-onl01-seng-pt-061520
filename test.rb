3e99e512949cb594de4ae04a5f5c271d9e3c1e7a
Charlie Barth <charliepbarth@gmail.com>
2020-06-26 18:04:28 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,9 +1,9 @@
 
 def swap_elements_from_to(array)
   array.each do |a|
-    if array.index("#{a}") = 1
+    if array.index("#{a}") == 1
       array.index("#{a}") + 1 
-    elsif array.index("#{a}") = 2
+    elsif array.index("#{a}") == 2
       array.index("#{a}") + -1 
     else
     end
